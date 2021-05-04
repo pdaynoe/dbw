6715cdd8f49da8135f04ec7e8b892798c44644cf
pdaynoe <ganon00web.de>
2021-05-04 18:27:25 +0200

    Bug fixes x2

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@
 [ -f "$PWD"/dbw ] || echo "Error: No dbw file found"; exit 1
 
 # [ -d /usr/local/bin ] && sudo cp "$PWD"/dbw /usr/local/bin
-[ -d /usr/bin ]   && sudo cp "$PWD"/dbw /usr/bin
+[ -d /usr/bin ]   && sudo cp "$PWD"/dbw.sh /usr/bin/dbw
 
-[ -f "${XDG_CONFIG_HOME:-$HOME/.config}/dbwdb.db"  ] ||  cp "$PWD"/dbwdb.db  "${XDG_CONFIG_HOME:-$HOME/.config}"
+[ -f "${XDG_CONFIG_HOME:-$HOME/.config}/dbwdb.db"  ] ||  cp "$PWD"/example_database.db  "${XDG_CONFIG_HOME:-$HOME/.config}"
