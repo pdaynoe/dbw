b5e941b1a6028ca70aa38e0a4bf73b6a707d11fb
pdaynoe <ganon00@web.de>
2025-11-03 10:46:14 +0100

    [dev] generally optimize with agent

--- dbw.sh
+++ dbw.sh
@@ -21,9 +21,9 @@ DMENU="wofi --show dmenu"
 # ### found this on WWW, unable to find source
 # ### it encodes strings into urls format
 urlencode(){
-  declare str="$*"
-  declare encoded=""
-  declare i c x
+  local str="$*"
+  local encoded=""
+  local i c x
   for ((i=0; i<${#str}; i++ )); do
     c=${str:$i:1}
     case "$c" in
@@ -37,45 +37,57 @@ urlencode(){
 
 
 get_dbfile() {
-      # ### find suitable database file if DBFILE is unset
-      export DBFILE="${DBFILE:-${XDG_CONFIG_HOME:-$HOME/.config}/dbwdb.db}"
-      [ -f "$DBFILE" ] || { printf "\nNo database found\!\n" ; exit 1 ; }
+    # Find suitable database file if DBFILE is unset
+    export DBFILE="${DBFILE:-${XDG_CONFIG_HOME:-$HOME/.config}/dbwdb.db}"
+    [ -f "$DBFILE" ] || { printf "\nError: No database found\n" >&2; exit 1; }
 }
 
 get_input() {
-      # ### use awk & dmenu on supplied input, defines variable INPUT
-      INPUT=$(awk '{if(/#/){}else{printf ("%s\t\t-\t%s\n", $1, $2) }}' "$DBFILE" | $DMENU -i -p "Search/Browse")
-      [[ "$INPUT" == *Cancel* ]] && unset INPUT SEARCHTERM SEARCHKEY && exit 0
-      [ -z "$INPUT" ] && unset INPUT SEARCHTERM SEARCHKEY && exit 0
+    # Use awk & dmenu on supplied input, defines variable INPUT
+    INPUT=$(awk '{if(/#/){}else{printf ("%s\t\t-\t%s\n", $1, $2) }}' "$DBFILE" | $DMENU -i -p "Search/Browse")
+    
+    # Handle cancel or empty input
+    [[ "$INPUT" == *Cancel* ]] && { unset INPUT SEARCHTERM SEARCHKEY; exit 0; }
+    [ -z "$INPUT" ] && { unset INPUT SEARCHTERM SEARCHKEY; exit 0; }
 
-      # ### open url immediately if it contains http(s) or www
-      [[ "$INPUT" == *http* ]] || [[ "$INPUT" == *www*  ]] && goto_www
+    # Open url immediately if it contains http(s) or www
+    [[ "$INPUT" == *http* ]] || [[ "$INPUT" == *www* ]] && goto_www
 
-      SEARCHKEY="$(echo "$INPUT" | awk '{print $1}')"
-      SEARCHTERM="$(echo "$INPUT" | awk '{$1=""; print $0}' | awk '{$1=$1};1')"
+    SEARCHKEY="$(echo "$INPUT" | awk '{print $1}')"
+    SEARCHTERM="$(echo "$INPUT" | awk '{$1=""; print $0}' | awk '{$1=$1};1')"
 
-      # ### this is failsafe for if you tab through suggestions
-      [[ "$INPUT" == *$(printf '\t')* ]] && SEARCHTERM=""
+    # This is failsafe for if you tab through suggestions
+    [[ "$INPUT" == *$(printf '\t')* ]] && SEARCHTERM=""
 
-      DBENTRY="$(grep -m 1 -e "^$SEARCHKEY " "$DBFILE")"
-      # ### perform defaultkey search for nonexisting searchkey
-      [ -z "$DBENTRY" ] && SEARCHTERM=$INPUT SEARCHKEY=${DEFKEY:-dg}\
-          DBENTRY="$(grep -m 1 -e "^$SEARCHKEY " "$DBFILE")"
+    DBENTRY="$(grep -m 1 -e "^$SEARCHKEY " "$DBFILE")"
+    
+    # Perform defaultkey search for nonexisting searchkey
+    if [ -z "$DBENTRY" ]; then
+        SEARCHTERM=$INPUT
+        SEARCHKEY=${DEFKEY:-dg}
+        DBENTRY="$(grep -m 1 -e "^$SEARCHKEY " "$DBFILE")"
+    fi
 }
 
 goto_www(){
-      BROWSER=${BROWSER:-xdg-open}
-      "$BROWSER" "$INPUT" & unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO & exit 0
+    BROWSER=${BROWSER:-xdg-open}
+    "$BROWSER" "$INPUT" & unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO & exit 0
 }
 
 
 goto_bmark() {
-            BMARK="$(echo "$DBENTRY" | awk '{print $4}')"
-            [ "$BMARK" = '-' ] && DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s.%s", $2,$3) }}' )" \
-                  || DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s.%s%s", $2,$3,$4) }}' )";
-                  # || DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s.%s%s", $2,$3,$4) }}' )";
-            [[ "$SEARCHKEY" = 'sx' ]] && DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s", $2) }}' )";
-            GOTO="$DOMAIN"
+    local BMARK="$(echo "$DBENTRY" | awk '{print $4}')"
+    
+    if [ "$BMARK" = '-' ]; then
+        DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s.%s", $2,$3) }}' )"
+    else
+        DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s.%s%s", $2,$3,$4) }}' )"
+    fi
+    
+    # Special handling for searxng instances
+    [[ "$SEARCHKEY" = 'sx' ]] && DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s", $2) }}' )"
+    
+    GOTO="$DOMAIN"
 }
 
 
@@ -93,34 +105,26 @@ full_search() {
 
 
 run() {
-      # ### the actual run function
-      BROWSER=${BROWSER:-xdg-open}
-      "$BROWSER" "$GOTO" && unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO && exit 0
+    # ### the actual run function
+    BROWSER=${BROWSER:-xdg-open}
+    "$BROWSER" "$GOTO" && unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO && exit 0
 }
 
+# Main execution
+main() {
+    [ -z "$DBFILE" ] && get_dbfile
+    
+    get_input  # this uses dmenu/rofi
+    
+    if [ -z "$SEARCHTERM" ]; then
+        goto_bmark
+        run
+    else
+        full_search
+        run
+    fi
+}
 
-# ### main
-[ -z "$DBFILE" ] && get_dbfile;
-
-get_input; # this uses dmenu/rofi
-
-if [ -z "$SEARCHTERM" ]; then
-      goto_bmark
-      run
-  else
-      full_search
-      run
-fi
-
-# [ -z "$SEARCHTERM" ] && goto_bmark|| goto_bmark; run
-
-
-# if [ -z "$SEARCHTERM" ]; then
-#       goto_bmark
-#       run
-
-# elif [ -n "$SEARCHTERM" ] ; then
-#       full_search
-#       run
-# fi
+# Execute main function
+main
 
