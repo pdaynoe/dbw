0e791237c677f4440bca0d50729ac398ecf9a2d7
pdaynoe <ganon00@web.de>
2025-11-03 17:49:46 +0100

    [dev] make posix compliant with agent

--- dbw.sh
+++ dbw.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 ######################################################################
 # @author      : pdaynoe
 # @requirements: base-devel, dmenu
@@ -18,25 +18,33 @@ DMENU="wofi --show dmenu"
 
 # ### found this on WWW, unable to find source
 # ### it encodes strings into urls format
-urlencode(){
-  local str="$*"
-  local encoded=""
-  local i c x
-  for ((i=0; i<${#str}; i++ )); do
-    c=${str:$i:1}
-    case "$c" in
-      [-_.~a-zA-Z0-9] ) x="$c" ;;
-      * ) printf -v x '%%%02x' "'$c";;
-    esac
-    encoded+="$x"
-  done
-  echo "$encoded"
+urlencode() {
+    str="$1"
+    encoded=""
+    i=0
+    str_len=$(expr length "$str")
+    while [ "$i" -lt "$str_len" ]; do
+        c=$(expr substr "$str" $((i + 1)) 1)
+        case "$c" in
+            [a-zA-Z0-9._~-] ) x="$c" ;;
+            ' ' ) x='%20' ;;
+            * ) 
+                # Use printf with proper quoting for POSIX compatibility
+                x=$(printf '%%%.2x' "'$c")
+                ;;
+        esac
+        encoded="$encoded$x"
+        i=$((i + 1))
+    done
+    echo "$encoded"
 }
 
 # Get database file
 get_dbfile() {
     # Find suitable database file if DBFILE is unset
-    export DBFILE="${DBFILE:-${XDG_CONFIG_HOME:-$HOME/.config}/dbwdb.db}"
+    if [ -z "$DBFILE" ]; then
+        DBFILE="${XDG_CONFIG_HOME:-$HOME/.config}/dbwdb.db"
+    fi
     if [ ! -f "$DBFILE" ]; then
         printf "\nError: No database found at %s\n" "$DBFILE" >&2
         exit 1
@@ -46,35 +54,48 @@ get_dbfile() {
 # Get user input
 get_input() {
     # Use awk & dmenu on supplied input, defines variable INPUT
-    INPUT=$(awk '{if(/#/){}else{printf ("%s\t\t-\t%s\n", $1, $2) }}' "$DBFILE" | $DMENU -i -p "Search/Browse")
+    INPUT="$(awk '{if(/#/){}else{printf ("%s\t\t-\t%s\n", $1, $2) }}' "$DBFILE" | $DMENU -i -p "Search/Browse")"
     
     # Handle cancel or empty input
-    if [[ "$INPUT" == *Cancel* ]] || [ -z "$INPUT" ]; then
-        unset INPUT SEARCHTERM SEARCHKEY
-        exit 0
-    fi
+    case "$INPUT" in
+        *Cancel*)
+            unset INPUT SEARCHTERM SEARCHKEY
+            exit 0
+            ;;
+        "")
+            unset INPUT SEARCHTERM SEARCHKEY
+            exit 0
+            ;;
+    esac
 
     # Open url immediately if it contains http(s) or www
-    if [[ "$INPUT" == *http* ]] || [[ "$INPUT" == *www* ]]; then
-        goto_www
-    fi
+    case "$INPUT" in
+        *http*)
+            goto_www
+            ;;
+        *www*)
+            goto_www
+            ;;
+    esac
 
     # Parse input
     SEARCHKEY="$(echo "$INPUT" | awk '{print $1}')"
     SEARCHTERM="$(echo "$INPUT" | awk '{$1=""; print $0}' | awk '{$1=$1};1')"
 
     # This is failsafe for if you tab through suggestions
-    if [[ "$INPUT" == *$(printf '\t')* ]]; then
-        SEARCHTERM=""
-    fi
+    case "$INPUT" in
+        *$(printf '\t')*)
+            SEARCHTERM=""
+            ;;
+    esac
 
     # Look up the database entry
     DBENTRY="$(grep -m 1 -e "^$SEARCHKEY " "$DBFILE")"
     
     # Perform defaultkey search for nonexisting searchkey
     if [ -z "$DBENTRY" ]; then
-        SEARCHTERM=$INPUT
-        SEARCHKEY=${DEFKEY:-dg}
+        SEARCHTERM="$INPUT"
+        SEARCHKEY="${DEFKEY:-dg}"
         DBENTRY="$(grep -m 1 -e "^$SEARCHKEY " "$DBFILE")"
     fi
     
@@ -86,57 +107,68 @@ get_input() {
 }
 
 # Open URL directly
-goto_www(){
-    BROWSER=${BROWSER:-xdg-open}
-    "$BROWSER" "$INPUT" & 
-    unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO 
+goto_www() {
+    BROWSER="${BROWSER:-xdg-open}"
+    "$BROWSER" "$INPUT"
+    unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO
     exit 0
 }
 
 # Handle bookmark access
 goto_bmark() {
-    local BMARK="$(echo "$DBENTRY" | awk '{print $4}')"
+    BMARK="$(echo "$DBENTRY" | awk '{print $4}')"
     
-    if [ "$BMARK" = '-' ]; then
+    if [ "$BMARK" = "-" ]; then
         DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s.%s", $2,$3) }}' )"
     else
         DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s.%s%s", $2,$3,$4) }}' )"
     fi
     
     # Special handling for searxng instances
-    if [[ "$SEARCHKEY" = 'sx' ]]; then
-        DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s", $2) }}' )"
-    fi
+    case "$SEARCHKEY" in
+        sx)
+            DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s", $2) }}' )"
+            ;;
+    esac
     
     GOTO="$DOMAIN"
 }
 
-# Process full search
+# Process full search - implement proper $SEARCHTERM replacement
 full_search() {
     # Determine domain based on search key
-    if [[ "$SEARCHKEY" = 'sx' ]]; then
-        DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s%s", $2,$5) }}' )"
-    else
-        DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s.%s%s", $2,$3,$5) }}' )"
-    fi
+    case "$SEARCHKEY" in
+        sx)
+            DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("http://%s%s", $2,$5) }}' )"
+            ;;
+        *)
+            DOMAIN="$(echo "$DBENTRY" | awk '{if(/#/){}else{printf ("https://%s.%s%s", $2,$3,$5) }}' )"
+            ;;
+    esac
     
     # Encode search term
-    SEARCHTERM=$(urlencode "$SEARCHTERM")
+    SEARCHTERM="$(urlencode "$SEARCHTERM")"
     
     # Get search end part
-    SEARCHEND=$(echo "$DBENTRY" | awk '{print $6}')
+    SEARCHEND="$(echo "$DBENTRY" | awk '{print $6}')"
     
     # Handle empty search end
-    if [[ "$SEARCHEND" = '-' ]]; then
+    if [ "$SEARCHEND" = "-" ]; then
         SEARCHEND=""
     fi
     
-    # Properly substitute $SEARCHTERM in the URL
-    GOTO="${DOMAIN/\$SEARCHTERM/$SEARCHTERM}$SEARCHEND"
-    
-    # Fallback in case substitution didn't work
-    if [[ "$GOTO" == *"\$SEARCHTERM"* ]]; then
-        GOTO="${DOMAIN/\$SEARCHTERM/$SEARCHTERM}$SEARCHEND"
+    # Properly substitute $SEARCHTERM in the URL using POSIX-compatible method
+    # Since POSIX doesn't support advanced string substitution, we'll use sed
+    # Check if the domain contains literal $SEARCHTERM pattern
+    if echo "$DOMAIN" | grep -q "\\\\$SEARCHTERM"; then
+        # Use sed to replace literal $SEARCHTERM with actual search term
+        GOTO="$(echo "$DOMAIN" | sed "s/\\\\\$SEARCHTERM/$SEARCHTERM/g")$SEARCHEND"
+    elif echo "$DOMAIN" | grep -q '\$SEARCHTERM'; then
+        # Use sed to replace literal $SEARCHTERM with actual search term
+        GOTO="$(echo "$DOMAIN" | sed "s/\$SEARCHTERM/$SEARCHTERM/g")$SEARCHEND"
+    else
+        # No special replacement needed, just concatenate
+        GOTO="$DOMAIN$SEARCHEND"
     fi
 }
 
@@ -149,9 +181,9 @@ run() {
     fi
     
     # Open in browser
-    BROWSER=${BROWSER:-xdg-open}
-    "$BROWSER" "$GOTO" 
-    unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO 
+    BROWSER="${BROWSER:-xdg-open}"
+    "$BROWSER" "$GOTO"
+    unset SEARCHKEY SEARCHTERM SEARCHEND DBENTRY DOMAIN GOTO
     exit 0
 }
 
