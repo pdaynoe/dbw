4d969556a97c41deac6696cd8e98e00f73849be3
ganon <ganon@disroot.org>
2026-02-08 01:43:13 +0100

    [dev] 2026-02-08_01:43:13

--- dbw.sh
+++ dbw.sh
@@ -16,7 +16,7 @@ DEFKEY=sx
 # Menu configuration
 # DMENU="wofi --show dmenu"
 # DMENU="rofi -dmenu -show-icons"
-DMENU="rofi -dmenu"
+DMENU="rofi -dmenu -no-show-icons"
 
 # ### found this on WWW, unable to find source
 # ### it encodes strings into urls format
